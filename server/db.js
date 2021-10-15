1f9cc288d926951160694654eaab409c7d6dc560
Chasezor <chase.c.contact@gmail.com>
2021-10-15 12:04:03 -0600

    saving

---
+++ server/db.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const quests= []
+const messages = "Quest Started"
+
+
+
+module.exports = {
+    quest: (req, res) => {
+
+    const{fate, guide, item} = req.body
+
+    const registeredQuest = {fate, guide, item}
+
+
+      //quests.push(registeredQuest)
+      quests.push(registeredQuest)
+
+
+    if(registeredQuest){
+        const myJSON = JSON.stringify(quests);
+        fs.writeFile(`quest.json`, myJSON, (err) => {
+      
+          // In case of a error throw err.
+          if (err) throw err;
+        })
+        }
+    res.status(200).send(quests)
+    console.log(quests)
+    console.log(messages)
+}
+}
---
+++ server/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+
+const {
+  quest
+} = require('./db')
+
+
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+
+
+app.get("/api/fortune", (req, res) => {
+  const compliments = 
+  [        "A beautiful, smart, and loving person will be coming into your life.",
+					 "A lifetime of happiness lies ahead of you.",
+					 "Believe in yourself and others will too.",
+           "If you wish to see the best in others, show the best of yourself.",
+           "It is honorable to stand up for what is right, however unpopular it seems."
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+
+app.post(`/api/quest`, quest)
+
+
+
+
+app.listen(4000, () => console.log("Server running on 4000"));
